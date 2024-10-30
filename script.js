c62ff9b81836966c89f5c6fe878316973e3e8fae
ameya180 <83203352+ameya180@users.noreply.github.com>
2024-10-30 22:20:19 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function revealMessage() {
-    document.getElementById("surprise-message").classList.remove("hidden");
+function revealBirthdayCountdown() {
+    const birthdayMessage = document.getElementById("birthday-message");
+    birthdayMessage.classList.toggle("hidden");
 }
